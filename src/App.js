f15f24d203c6e7037a799e115ec4b854ebe37127
Miguel Ángel Mireles Vázquez <mamv3x3@gmail.com>
2024-03-10 01:41:08 -0600

    feat(components): move Modal.js and Modal.css to Modal directory

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Modal from "./components/Modal";
+import Modal from "./components/Modal/Modal";
 import AddSkillForm from "./views/Skills/AddSkillForm";
 import "./App.css";
 
